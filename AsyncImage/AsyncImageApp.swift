01cc09feb121d82575456254efa86be0f64e20ab
Alfonso Patron <c9alfonsopatron@hotmail.com>
2024-01-03 21:19:04 -0700

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Alfonso Patron on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
