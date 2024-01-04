d56296fba72186f65501c37a8e0c89259ee94199
Alfonso Patron <c9alfonsopatron@hotmail.com>
2024-01-03 21:34:34 -0700

    Add 2 Ways - Basic - Scale

--- AsyncImage/ContentView.swift
+++ AsyncImage/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let  imageURL : String = "https://credo.academy/credo-academy@3x.png"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        //MARK: -1. BASIC -> Fast and Easy to show a image in AsyncImage.
+
+        // AsyncImage(url: URL(string: imageURL))
+        
+        //MARK: -2. SCALE -> Most Manipulable
+        AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+        
     }
 }
 
